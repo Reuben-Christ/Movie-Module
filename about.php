10e1de1ef4c25dcc5dda7c2e1b66f06b746f213d
Reuben-Christ <96035781+Reuben-Christ@users.noreply.github.com>
2021-12-22 16:33:19 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style1.css"> 
+	</head>
+  <?php
+include 'header.php';
+?>
+  <center>
+<div  style="width:95vw;height:42vw; margin-top:2vw;margin-left:-1vw;background-color:#dbdeca; border:0.3vw  #06390d;">
+<div style="position:absolute;margin-top: 0.5vw;margin-right: 1vw;margin-left: 1vw;text-align: left;">
+  <center><h1 style="color:black; font-size: 3vw;"> About Us </h1></center>
+  <p style="font-family:verdana;color:black; font-size:1.8vw; font-style: bold;">
+
+    <p style="font-family:verdana; text-align: left; color:black; font-size:1.3vw;">We at movie hub provide you and your family the best movie watching experience in Koramangala.
+
+We have the fastest ticket booking system, reserve the best seats with your loved ones before they run out!!!
+</p>
+
+<p style="font-family:verdana; text-align: left; color:black; font-size:1.3vw;">No need of carryig the print of the tickets to the cinemas as we provide you with an e-ticket at which is sent to you right after the booking process is done.</p>
+
+<p style="font-family:verdana; text-align: left;color:black;font-size:1.3vw;">Enjoy your self with our delicious range of snacks both veg and non-veg  such as 3 variants of popcorns, burgers, hamburgers, kebabs,hotdogs, nachos,soft-drinks,etc.
+</p>
+
+<p style="font-family:verdana; text-align: left;color:black;font-size:1.3vw;">To improve your experience we even deliver your snacks to the seat if you inform us before your show starts.</p>
+
+<p style="font-family:verdana; text-align: left;color:black;font-size:1.3vw;">All our staff is vaccinated and temperratures are checked periodically and all the premises are sanitized from time to time for your safety!!!</p>
+
+<p style="font-family:verdana; text-align: left;color:black;font-size:1.3vw;">For any query please contact movieshub@gmail.com.</p>
+</div>
+</div>
+</center>
+</body>
+<style>
+.footer{
+  background-color:black;
+  font-size: 1.3vw;
+  width:100vw;
+  margin-left:-1vw;
+  margin-top:20vw;
+  margin-bottom: -1vw;
+  height: 5vw;
+  text-align:center;
+  color:white;
+}
+  </style>
+  <body>
+<?php
+include 'footer.php';
+?>
+</body>
